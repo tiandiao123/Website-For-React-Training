474094fb8c851a92b1d878689fbde14abcde4c0d
Cuiqing Li <lixx3527@gmail.com>
2017-06-22 23:40:10 -0400

    fix bugs

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,13 @@
-const title = React.createElement(
-    'h1',
-    {id:'title',className: 'header'},
-    'Hello WOrld'
+const { createElement } = React
+const { render } = ReactDOM
+
+const title = createElement(
+	'h1',
+	{id: 'title', className: 'header'},
+	'Hello World'
 )
 
-ReactDOM.render(
-    title,
-    document.getElementById('react-container')
+render(
+	title,
+	document.getElementById('react-container')
 )
