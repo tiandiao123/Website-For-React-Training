f512845c9ca054e168bbdb3a67b5eef72d10d1c8
Cuiqing Li <lixx3527@gmail.com>
2017-06-22 23:48:29 -0400

    change background coloe

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,16 @@
 const { createElement } = React
 const { render } = ReactDOM
 
+const style = {
+    backgroundColor: 'orange',
+    color: 'white',
+    fontFamily: 'verdana'
+}
+
+
 const title = createElement(
 	'h1',
-	{id: 'title', className: 'header'},
+	{id: 'title', className: 'header',style:style},
 	'Hello World'
 )
 
